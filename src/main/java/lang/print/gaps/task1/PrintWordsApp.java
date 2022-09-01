11a06beb6d64202f779de43db993f01b843e41d1
“Kirill <21itzd.podgol.k@pdu.by>
2022-09-02 00:34:31 +0300

    123

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,7 +2,10 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java" + "\n" + "is" + "\n" +"smth");
+        System.out.println("""
+                java
+                is
+                smth""");
 
     }
 }
